87175942f27f0a7d17e61487e1a7d38acb778e1e
Colin Patrick Mccabe <cmccabe@cloudera.com>
2013-04-01 14:00:46 -0700

    add getpwuid test, make it possible to run both tests (or neither)
    Signed-off-by: Colin Patrick McCabe <cmccabe@cloudera.com>

--- main.c
+++ main.c
@@ -1,3 +1,21 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #include "hadoop_group_info.h"
 #include "hadoop_user_info.h"
 
@@ -13,6 +31,8 @@ struct options {
   int use_reentrant;
   int verbose;
   int num_threads;
+  int run_getgrgid_test;
+  int run_getpwduid_test;
 };
 
 static struct options g_options;
@@ -32,9 +52,13 @@ static void usage(void)
   fprintf(stderr, "unix-groups-reentrancy-test\n\n");
   fprintf(stderr, "options:\n");
   fprintf(stderr, 
+    "-g:             run getgrgid test\n");
+  fprintf(stderr, 
     "-h:             this help message.\n");
   fprintf(stderr, 
     "-n:             use the non-reentrant versions of the functions\n");
+  fprintf(stderr, 
+    "-p:             run getpwuid test\n");
   fprintf(stderr, 
     "-v:             be verbose\n");
 }
@@ -93,7 +117,7 @@ static int find_all_uids(void)
     if (ret != 0)
       continue;
     n_user_ids = realloc(g_user_ids,
-        sizeof(gid_t) * (g_num_user_ids + 1));
+        sizeof(uid_t) * (g_num_user_ids + 1));
     if (!n_user_ids) {
       fprintf(stderr, "failed to reallocate an array of size %d\n",
               g_num_user_ids + 1);
@@ -124,7 +148,7 @@ static void *test_gids(void *v)
     goto done;
   }
   while (1) {
-    for (i = 0; i <= g_num_group_ids; i++) {
+    for (i = 0; i < g_num_group_ids; i++) {
       ret = hadoop_group_info_fetch(ginfo, g_group_ids[i]);
       if (ret != 0) {
         pthread_mutex_lock(&g_strerror_lock);
@@ -135,7 +159,7 @@ static void *test_gids(void *v)
       }
       iter++;
       if (iter == 1000) {
-        fprintf(stderr, ".");
+        fprintf(stderr, "g");
         iter = 0;
       }
     }
@@ -143,6 +167,43 @@ static void *test_gids(void *v)
 done:
   if (ginfo) {
     hadoop_group_info_free(ginfo);
+  }
+  return (void*)(uintptr_t)ret;
+}
+
+static void *test_uids(void *v)
+{
+  int i, iter = 0;
+  struct hadoop_user_info *uinfo = NULL;
+  int ret = 0;
+
+  uinfo = hadoop_user_info_alloc(g_options.use_reentrant);
+  if (!uinfo) {
+    fprintf(stderr, "hadoop_user_info_alloc failed.  v=%p\n", v);
+    ret = ENOMEM;
+    goto done;
+  }
+  while (1) {
+    for (i = 0; i < g_num_user_ids; i++) {
+      ret = hadoop_user_id_info_fetch(uinfo, g_user_ids[i]);
+      if (ret != 0) {
+        pthread_mutex_lock(&g_strerror_lock);
+        fprintf(stderr, "hadoop_user_info_fetch: error %d (%s) when "
+                "looking up uid %"PRId64"\n",
+                ret, strerror(ret), (uint64_t)g_user_ids[i]);
+        pthread_mutex_unlock(&g_strerror_lock);
+        break;
+      }
+      iter++;
+      if (iter == 1000) {
+        fprintf(stderr, "u");
+        iter = 0;
+      }
+    }
+  }
+done:
+  if (uinfo) {
+    hadoop_user_info_free(uinfo);
   }
   return (void*)(uintptr_t)ret;
 }
@@ -155,14 +216,20 @@ static void get_options(struct options *opts, int argc, char **argv)
   opts->use_reentrant = 1;
   opts->num_threads = 10;
 
-  while ((c = getopt (argc, argv, "hnt:v")) != -1) {
+  while ((c = getopt (argc, argv, "ghnpt:v")) != -1) {
     switch (c) {
+    case 'g':
+      opts->run_getgrgid_test = 1;
+      break;
     case 'h':
       usage();
       exit(0);
       break;
     case 'n':
       opts->use_reentrant = 0;
+      break;
+    case 'p':
+      opts->run_getpwduid_test = 1;
       break;
     case 't':
       opts->num_threads = atoi(optarg);
@@ -239,45 +306,59 @@ static void join_threads(pthread_t *threads, int nthreads)
 int main(int argc, char **argv)
 {
   int i, ret;
-  pthread_t *threads;
+  pthread_t *pw_threads = NULL, *gr_threads = NULL;
 
   get_options(&g_options, argc, argv);
-  fprintf(stderr, "locating all groups on system...\n");
-  ret = find_all_gids();
-  if (ret) {
-    fprintf(stderr, "failed to find all gids: error %d (%s)\n",
-            ret, strerror(ret));
-    exit(EXIT_FAILURE);
-  }
-  if (g_options.verbose) {
-    const char *prefix = "";
-    fprintf(stderr, "found group IDs: ");
-    for (i = 0; i < g_num_group_ids; i++) {
-      fprintf(stderr, "%s%" PRId64, prefix, (uint64_t)g_group_ids[i]);
-      prefix = ", ";
+  if (g_options.run_getgrgid_test) {
+    fprintf(stderr, "locating all groups on system...\n");
+    ret = find_all_gids();
+    if (ret) {
+      fprintf(stderr, "failed to find all gids: error %d (%s)\n",
+              ret, strerror(ret));
+      exit(EXIT_FAILURE);
     }
-    fprintf(stderr, "\n");
-  }
-  fprintf(stderr, "locating all users on system...\n");
-  ret = find_all_uids();
-  if (ret) {
-    fprintf(stderr, "failed to find all uids: error %d (%s)\n",
-            ret, strerror(ret));
-    exit(EXIT_FAILURE);
-  }
-  if (g_options.verbose) {
-    const char *prefix = "";
-    fprintf(stderr, "found user IDs: ");
-    for (i = 0; i < g_num_user_ids; i++) {
-      fprintf(stderr, "%s%" PRId64, prefix, (uint64_t)g_user_ids[i]);
-      prefix = ", ";
+    if (g_options.verbose) {
+      const char *prefix = "";
+      fprintf(stderr, "found group IDs: ");
+      for (i = 0; i < g_num_group_ids; i++) {
+        fprintf(stderr, "%s%" PRId64, prefix, (uint64_t)g_group_ids[i]);
+        prefix = ", ";
+      }
+      fprintf(stderr, "\n");
     }
-    fprintf(stderr, "\n");
   }
-  fprintf(stderr, "starting test threads...\n");
-  run_test(&threads, g_options.num_threads, test_gids);
-
-  join_threads(threads, g_options.num_threads);
+  if (g_options.run_getpwduid_test) {
+    fprintf(stderr, "locating all users on system...\n");
+    ret = find_all_uids();
+    if (ret) {
+      fprintf(stderr, "failed to find all uids: error %d (%s)\n",
+              ret, strerror(ret));
+      exit(EXIT_FAILURE);
+    }
+    if (g_options.verbose) {
+      const char *prefix = "";
+      fprintf(stderr, "found user IDs: ");
+      for (i = 0; i < g_num_user_ids; i++) {
+        fprintf(stderr, "%s%" PRId64, prefix, (uint64_t)g_user_ids[i]);
+        prefix = ", ";
+      }
+      fprintf(stderr, "\n");
+    }
+  }
+  if (g_options.run_getgrgid_test) {
+    fprintf(stderr, "starting getgrgid test threads...\n");
+    run_test(&gr_threads, g_options.num_threads, test_gids);
+  }
+  if (g_options.run_getpwduid_test) {
+    fprintf(stderr, "starting getpwuid test threads...\n");
+    run_test(&pw_threads, g_options.num_threads, test_uids);
+  }
+  if (g_options.run_getgrgid_test) {
+    join_threads(gr_threads, g_options.num_threads);
+  }
+  if (g_options.run_getpwduid_test) {
+    join_threads(pw_threads, g_options.num_threads);
+  }
 
   return 0;
 }
